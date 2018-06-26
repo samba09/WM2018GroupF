53d5e388a19bf1297517ee5e250d17739dc532ad
Gerhard Waidelich <gerhardwaidelich@gmail.com>
2018-06-26 16:23:16 +0200

    calculated the real FIFA rules now (except fairplay).

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # WM2018GroupF
 Germanys change to win the GroupF
+
+the rules like FIFA 2018:
+
+First step:
+
+Pursuant to the criteria listed in art. 32 (5) lit. a) to c) of the Competition Regulations.
+
+greatest number of points obtained in all group matches;
+goal difference in all group matches;
+greatest number of goals scored in all group matches.
+Second step:
+
+If two or more teams are equal on the basis of the first step (see example in Table 1), their ranking will be determined by applying to the group matches between the teams concerned the criteria listed in art. 32 (5) lit. d) to h) in the order of their listing.
+
+greatest number of points obtained in the group matches between the teams concerned;
+goal difference resulting from the group matches between the teams concerned;
+greater number of goals scored in all group matches between the teams concerned;
+greater number of points obtained in the fair play conduct of the teams based on yellow and red cards received in all group matches;
+drawing of lots by the FIFA.
+
+
--- Wm2018GroupF.py
+++ Wm2018GroupF.py
@@ -5,48 +5,114 @@ from mpl_toolkits.mplot3d import Axes3D
 eqip = ["GER", "SWE", "MEX", "KOR"]
 status = [[(), (2, 1), (0, 1), ()], [(), (), (), (1, 0)], [(), (), (), (2, 1)], [(), (), (), ()]]
 
+class teamStatus:
+
+    points = 0
+    diffGoals = 0
+    goals = 0
+    sumGoals = 0
+    name = ""
+    def __init__(self, n):
+        self.name = n
+
+    def toString(self):
+        return self.name + ", " + str(self.goals) + ", " +str(self.diffGoals) + ", " + str(self.points)
 
 
-def calcPlaceD():
-
-    teamStatus = [[0, 0], [0, 0], [0, 0], [0, 0]]
-    for i in range(4):
-        for j in range(4):
+def calcTable(table):
+    for i in range(len(table)):
+        for j in range(len(table)):
             g = status[i][j]
             if len(g) != 0:
                 d = g[0] - g[1]
-                teamStatus[i][0] += d
-                teamStatus[j][0] -= d
+                table[i].goals += g[0]
+                table[j].goals += g[1]
+                table[i].diffGoals += d
+                table[j].diffGoals -= d
                 p0 = 3 if (g[0] > g[1]) else 1 if g[0] == g[1] else 0
                 p1 = 3 if (g[1] > g[0]) else 1 if g[0] == g[1] else 0
-                teamStatus[i][1] += p0
-                teamStatus[j][1] += p1
+                table[i].points += p0
+                table[j].points += p1
 
-    l = sorted(range(len(teamStatus)), key=lambda k: teamStatus[k][1] * 100 + teamStatus[k][0])
-    pos = 3-l.index(0)
+
+def calcRank(table, k):
+    rank = table[k].points * 100000 + table[k].diffGoals * 1000 + table[k].goals * 10
+    return rank
+
+#def calcDirektCompare(k1,k2):
+
+def calcPlaceGER(table, norecursive = False):
+
+    i = sorted(range(len(table)), key=lambda k: calcRank(table, k), reverse=True)
+    pos = i.index(0)
+
+    equallist = []
+    for j in range(1,len(table)):
+        if calcRank(table,0) == calcRank(table, j):
+            equallist.append((j, i.index(j)))
+
+    if len(equallist) == 0:
+        return pos
+
+    if norecursive:
+        print("Fair play needed!")
+        return pos
+
+    equallist.insert(0,(0, pos))
+
+    team = [i for i,pos in equallist]
+    minitable = [teamStatus(eqip[i]) for i in team]
+    calcTable(minitable)
+    # print table
+    print("minitable:")
+    for e in minitable:
+        print(e.toString())
+    pos = calcPlaceGER(minitable, True) + min([e[1] for e in equallist])
+    print("more eqal", equallist, "pl." , min([e[1] for e in equallist]), "ger:", pos)
+
+
+
+
+
 
     return pos
 
 
-placeD = calcPlaceD()
-print("GER now", placeD)
+
+
+result = np.empty([14,14])
+
+for i0 in range(7):
+    for j0 in range(7):
+        for i1 in range(7):
+            for j1 in range(7):
+                status[0][3] = (i0,j0)  ## GER -- KOR
+                status[1][2] = (i1,j1)  ## SWE -- MEX
+                table = [teamStatus(eqip[ii]) for ii in range(4)]
+                calcTable(table)
+
+                # print table
+                for ii in range(4):
+                    print(table[ii].toString())
+
+                placeGER = calcPlaceGER(table)
+                print("GER--KOR dif:", i0-j0, "SWE--MEX diff:", i1-j1, "is GER: ", placeGER + 1)
+                x = i0-j0+6
+                y = i1-j1+6
+                result[x, y] = max(placeGER, result[x, y])
 
 dz = []
 x = []
 y = []
 color = []
+for i in range(4,11):
+    for j in range (4,11):
 
-for i in range(7):
-    for j in range(7):
-        status[0][3] = (3 - i, 0)  ## GER -- KOR
-        status[1][2] = (3 - j, 0)  ## SWE -- MEX
-        placeD = calcPlaceD()
-        print("GER--KOR dif:", 3 - i, "SWE--MEX diff:", 3 - j, "is GER: ", placeD + 1)
-        dz.append(placeD+1)
-        c = '#00ceaa' if placeD <= 1 else '#ceaa00'
+        dz.append(result[i, j]+1)
+        c = '#00ceaa' if result[i, j] <= 1 else '#ceaa00'
         color.append(c)
-        x.append(3 - i)
-        y.append(3 - j)
+        x.append(i-6)
+        y.append(j-6)
 
 # setup the figure and axes
 fig = plt.figure(figsize=(8, 6))
@@ -56,7 +122,7 @@ dy = np.ones(len(x)) / 2
 z = np.zeros(len(x))
 
 ax1.bar3d(x, y, z, dx, dy, dz, color, shade=True)
-ax1.view_init(ax1.elev, ax1.azim + 90)
+#ax1.view_init(ax1.elev, ax1.azim + 90)
 ax1.set_xlabel("GER--KOR")
 ax1.set_ylabel("SWE--MEX")
 ax1.set_zticks([1,2,3,4])
